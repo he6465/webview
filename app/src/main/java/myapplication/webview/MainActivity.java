8d3807975365cf0aea389f95fad473afeb13ded3
qwerqwer <qwerqwerqwe>
2015-11-05 10:12:03 +0900

    Initial commit

---
+++ app/src/main/java/myapplication/webview/MainActivity.java
@@ -0,0 +1,81 @@
+package myapplication.webview;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+    EditText edtUrl;
+    Button btnGo, btnBack;
+    WebView web;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtUrl = (EditText) findViewById(R.id.edtUrl);
+        btnGo = (Button) findViewById(R.id.btnGo);
+        btnBack = (Button) findViewById(R.id.btnBack);
+        web = (WebView) findViewById(R.id.webView1);
+
+        web.setWebViewClient(new CookWebViewClient());
+
+        WebSettings webSet = web.getSettings();
+        webSet.setBuiltInZoomControls(true);
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                    web.loadUrl(edtUrl.getText().toString());
+                }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                web.goBack();
+            }
+        });
+    }
+
+    class CookWebViewClient extends WebViewClient{
+        @Override
+        public  boolean shouldOverrideUrlLoading(WebView view, String url){
+            return false;
+        }
+        @Override
+        public void onPageFinished(WebView view, String url){
+            edtUrl.setText(view.getRootView().toString());
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
